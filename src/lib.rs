ebf43358662a34d9c03e1524c38f8f3359310dff
xpanvictor <xpanvictor@gmail.com>
2024-08-08 19:19:27 +0100

    feat(skeleton): skeleton to stop type errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct KvStore;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl KvStore {
+    pub fn new() -> KvStore {
+        KvStore{}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    pub fn get(&self, key: String) ->  Option<String> {
+        unimplemented!()
+    }
+
+    pub fn remove(&self, key: String) {
+        unimplemented!()
     }
 }
